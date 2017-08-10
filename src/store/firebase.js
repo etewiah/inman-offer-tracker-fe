82761247f81ad029884aad9b29202cbf6013fda6
Jess Martin <jessmartin@gmail.com>
2017-08-10 14:02:43 -0700

    Remove creds

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -3,12 +3,6 @@ import Firebase from 'firebase'
 
 const firebaseApp = Firebase.initializeApp({
   // settings go here
-  apiKey: 'AIzaSyC9w8lejfwh7BakjS4xb6F4tlVMaWwPXlA',
-  authDomain: 'offer-tracker.firebaseapp.com',
-  databaseURL: 'https://offer-tracker.firebaseio.com',
-  projectId: 'offer-tracker',
-  storageBucket: '',
-  messagingSenderId: '912478597249'
 });
 
 // Export the database for components to use.
