cac4cf30cfa64b7acb38d33055259d5151f3fa9f
Todd LaMothe <toddlamothe@MacBook-Pro-2.local>
2017-08-10 15:15:50 -0700

    Removed references to unused mainApp

--- src/main.js
+++ src/main.js
@@ -19,15 +19,3 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
-
-
-// import mainapp from './Main.vue';
-// import store from './store';
-
-// window.mainApp = new Vue({
-//   el: '#mainApp',
-//   store,
-//   components: { mainapp },
-// });
-
-
