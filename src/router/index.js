f54a320323a3e6a34a5fe3c86015d44466677ddc
Ed Tewiah <etewiah@hotmail.cim>
2017-08-11 00:46:03 +0200

    Import modules with relative paths

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import App from '@/App'
-import Offer from '@/components/offer'
-import Agent from '@/components/agent'
-import Login from '@/components/login'
+import App from '../App'
+import Offer from '../components/offer'
+import Agent from '../components/agent'
+import Login from '../components/login'
 
 Vue.use(Router)
 
