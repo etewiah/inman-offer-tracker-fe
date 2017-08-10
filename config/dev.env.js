ae670cba3419fe0bff4ae18c178f4d8764ec3e7e
Jess Martin <jessmartin@gmail.com>
2017-08-10 14:31:38 -0700

    Remove spurious env vars

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,7 +2,5 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  FIREBASE_API: 'val',
-  FIREBASE_SENDER: 'val'
+  NODE_ENV: '"development"'
 })
